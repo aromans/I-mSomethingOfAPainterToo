d60d021fa4d50c9899d89cb6724a4aca7c837f98
Andrew Romans <andrewromans@wsip-70-175-234-35.mc.at.cox.net>
2022-02-24 10:16:42 -0500

    Python file of notebook

---
+++ imapropainternow.py
@@ -0,0 +1,381 @@
+# Model Imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from math import log2
+
+# Util Imports
+import random
+import numpy as np
+import os
+
+import torchvision
+from torchvision.utils import save_image
+
+# Train Imports
+import wandb
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+factors = [1, 1, 1, 1, 1/2, 1/4, 1/8, 1/16, 1/32]
+START_TRAIN_AT_IMG_SIZE = 16
+DATASET = "/kaggle/working/images/"
+CHECKPOINT_GEN = "generator.pth"
+CHECKPOINT_CRITIC = "critic.pth"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+SAVE_MODEL = True
+LOAD_MODEL = True
+LEARNING_RATE = 1e-3
+BATCH_SIZES = [16, 16, 16, 16, 16, 16, 16, 8, 4]
+IMAGE_SIZE = 256
+CHANNELS_IMG = 3
+Z_DIM = 256
+IN_CHANNELS = 256
+LAMBDA_GP = 10
+NUM_STEPS = int(log2(IMAGE_SIZE / 4)) + 1
+
+PROGRESSIVE_EPOCHS = [100] * len(BATCH_SIZES)
+FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
+NUM_WORKERS = 4
+
+class WSConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, gain=2):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.scale = (gain / (in_channels * kernel_size ** 2)) ** 0.5
+        self.bias = self.conv.bias
+        self.conv.bias = None
+        
+        # initialize the conv layer
+        nn.init.normal_(self.conv.weight)
+        nn.init.zeros_(self.bias)
+        
+    def forward(self, x):
+        return self.conv(x * self.scale) + self.bias.view(1, self.bias.shape[0], 1, 1)
+
+class PixelNorm(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.epsilon = 1e-8
+        
+    def forward(self, x):
+        return x / torch.sqrt(torch.mean(x**2, dim=1, keepdim=True) + self.epsilon)
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, use_pixelnorm=True):
+        super().__init__()
+        self.conv1 = WSConv2d(in_channels, out_channels)
+        self.conv2 = WSConv2d(out_channels, out_channels)
+        self.leaky = nn.LeakyReLU(0.2)
+        self.pn = PixelNorm()
+        self.use_pn = use_pixelnorm
+        
+    def forward(self, x):
+        x = self.leaky(self.conv1(x))
+        x = self.pn(x) if self.use_pn else x
+        x = self.leaky(self.conv2(x))
+        x = self.pn(x) if self.use_pn else x
+        return x
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, in_channels, img_channels=3):
+        super().__init__()
+        self.initial = nn.Sequential(
+            PixelNorm(),
+            nn.ConvTranspose2d(z_dim, in_channels, 4, 1, 0), #1x1 -> 4x4
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+            PixelNorm()
+        )
+        
+        self.initial_rgb = WSConv2d(in_channels, img_channels, kernel_size=1, stride=1, padding=0)
+        self.prog_blocks, self.rgb_layers = nn.ModuleList(), nn.ModuleList([self.initial_rgb])
+        
+        for i in range(len(factors) - 1):
+            # factors[i] -> factors[i+1]
+            conv_in_c = int(in_channels * factors[i])
+            conv_out_c = int(in_channels * factors[i+1])
+            self.prog_blocks.append(ConvBlock(conv_in_c, conv_out_c))
+            self.rgb_layers.append(WSConv2d(conv_out_c, img_channels, kernel_size=1, stride=1, padding=0))
+    
+    def fade_in(self, alpha, upscaled, generated):
+        return torch.tanh(alpha * generated + (1-alpha) * upscaled)
+    
+    def forward(self, x, alpha, steps): #steps=0 (4x4), steps=1 (8x8), ....
+        out = self.initial(x) # 4x4
+        
+        if steps == 0:
+            return self.initial_rgb(out)
+        
+        for step in range(steps):
+            upscaled = F.interpolate(out, scale_factor=2, mode="nearest")
+            out = self.prog_blocks[step](upscaled)
+            
+        final_upscaled = self.rgb_layers[steps - 1](upscaled)
+        final_out = self.rgb_layers[steps](out)
+        return self.fade_in(alpha, final_upscaled, final_out)
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, img_channels=3):
+        super().__init__()
+        self.prog_blocks, self.rgb_layers = nn.ModuleList(), nn.ModuleList()
+        self.leaky = nn.LeakyReLU(0.2)
+        
+        for i in range(len(factors) - 1, 0, -1):
+            conv_in_c = int(in_channels * factors[i])
+            conv_out_c = int(in_channels * factors[i-1])
+            self.prog_blocks.append(ConvBlock(conv_in_c, conv_out_c, use_pixelnorm=False))
+            self.rgb_layers.append(WSConv2d(img_channels, conv_in_c, kernel_size=1, stride=1, padding=0))
+        
+        # Mirrors the 4x4 img resolution from generator
+        self.initial_rgb = WSConv2d(img_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.rgb_layers.append(self.initial_rgb)
+        self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
+        
+        # block for 4x4 resolution
+        self.final_block = nn.Sequential(
+            WSConv2d(in_channels+1, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=4, stride=1, padding=0),
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, 1, kernel_size=1, stride=1, padding=0)
+        )
+    
+    def fade_in(self, alpha, downscaled, out):
+        return alpha * out + (1 - alpha) * downscaled
+    
+    def minibatch_std(self, x):
+        batch_statistics = torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        return torch.cat([x, batch_statistics], dim=1) # 512 -> 513
+    
+    def forward(self, x, alpha, steps): # steps=0 (4x4), steps=1 (8x8), ....
+        cur_step = len(self.prog_blocks) - steps
+        out = self.leaky(self.rgb_layers[cur_step](x))
+        
+        if steps == 0:
+            out = self.minibatch_std(out)
+            return self.final_block(out).view(out.shape[0], -1)
+        
+        downscaled = self.leaky(self.rgb_layers[cur_step+1](self.avg_pool(x)))
+        out = self.avg_pool(self.prog_blocks[cur_step](out))
+        out = self.fade_in(alpha, downscaled, out)
+        
+        for step in range(cur_step + 1, len(self.prog_blocks)):
+            out = self.prog_blocks[step](out)
+            out = self.avg_pool(out)
+            
+        out = self.minibatch_std(out)
+        return self.final_block(out).view(out.shape[0], -1)
+
+def plot_to_wandb(loss_critic, loss_gen, real, fake):
+    wandb.log({
+        "Loss Critic": loss_critic,
+        "Loss Generator": loss_gen,
+    })
+    
+    with torch.no_grad():
+        img_grid_real = torchvision.utils.make_grid(real[:8], normalize=True)
+        img_grid_fake = torchvision.utils.make_grid(fake[:8], normalize=True)
+        wandb.log({
+            "Real Images": [wandb.Image(i) for i in img_grid_real],
+            "Fake Images": [wandb.Image(i) for i in img_grid_fake]
+        })
+
+def gradient_penalty(critic, real, fake, alpha, train_step, device="cpu"):
+    batch_size, c, h, w = real.shape
+    beta = torch.rand((batch_size, 1, 1, 1)).repeat(1, c, h, w).to(device)
+    
+    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images.requires_grad_(True)
+    
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_images, alpha, train_step)
+    
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving Checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict()
+    }
+    torch.save(checkpoint, filename)
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading Checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location="cuda")
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+def generate_examples(gen, steps, n=100):
+    gen.eval()
+    alpha = 1.0
+    for i in range(n):
+        with torch.no_grad():
+            noise = torch.randn(1, Z_DIM, 1, 1).to(DEVICE)
+            img = gen(noise, alpha, steps)
+            save_image(img * 0.5 + 0.5, f"saved_examples/img_{i}.png")
+            
+    gen.train()
+
+def get_loader(image_size):
+    transform = transforms.Compose(
+        [
+            transforms.Resize((image_size, image_size)),
+            transforms.ToTensor(),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.Normalize(
+                [0.5 for _ in range(CHANNELS_IMG)],
+                [0.5 for _ in range(CHANNELS_IMG)]
+            ),
+        ]
+    )
+    
+    batch_size = BATCH_SIZES[int(log2(image_size / 4))]
+    dataset = datasets.ImageFolder(root=DATASET, transform=transform)
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=NUM_WORKERS,
+        pin_memory=True,
+    )
+    return loader, dataset
+
+def train_fn(
+    critic,
+    gen,
+    loader,
+    dataset,
+    step,
+    alpha,
+    opt_critic,
+    opt_gen,
+    scalar_gen,
+    scalar_critic
+):
+    loop = tqdm(loader, leave=True)
+    for batch_idx, (real, _) in enumerate(loop):
+        real = real.to(DEVICE)
+        cur_batch_size = real.shape[0]
+        
+        # Train Critic: max E[critic(real) - E[critic(fake)]]
+        noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(DEVICE)
+        
+        with torch.cuda.amp.autocast():
+            fake = gen(noise, alpha, step)
+            critic_real = critic(real, alpha, step)
+            critic_fake = critic(fake.detach(), alpha, step)
+            gp = gradient_penalty(critic, real, fake, alpha, step, device=DEVICE)
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake))
+                + LAMBDA_GP * gp
+                + (0.001 * torch.mean(critic_real ** 2))
+            )
+            
+        opt_critic.zero_grad()
+        scalar_critic.scale(loss_critic).backward()
+        scalar_critic.step(opt_critic)
+        scalar_critic.update()
+        
+        # Train Generator: max E[critic(gen_fake)]
+        with torch.cuda.amp.autocast():
+            gen_fake = critic(fake, alpha, step)
+            loss_gen = -torch.mean(gen_fake)
+            
+        opt_gen.zero_grad()
+        scalar_gen.scale(loss_gen).backward()
+        scalar_gen.step(opt_gen)
+        scalar_gen.update()
+        
+        alpha += cur_batch_size / (len(dataset) * PROGRESSIVE_EPOCHS[step]*0.5)
+        alpha = min(alpha, 1)
+        
+        if batch_idx % 500 == 0:
+            with torch.no_grad():
+                fixed_fakes = gen(FIXED_NOISE, alpha, step) * 0.5 + 0.5
+            plot_to_wandb(
+                loss_critic.item(),
+                loss_gen.item(),
+                real.detach(),
+                fixed_fakes.detach()
+            )
+            
+    return alpha
+
+def main():
+    gen = Generator(
+        Z_DIM, IN_CHANNELS, CHANNELS_IMG
+    ).to(DEVICE)
+    critic = Discriminator(
+        IN_CHANNELS, CHANNELS_IMG
+    ).to(DEVICE)
+    
+    # initialize optimizers and scalars
+    opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.99))
+    opt_critic = optim.Adam(
+        critic.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.99)
+    )
+    scalar_critic = torch.cuda.amp.GradScaler()
+    scalar_gen = torch.cuda.amp.GradScaler()
+    
+    if LOAD_MODEL:
+        load_checkpoint(
+            CHECKPOINT_GEN, gen, opt_gen, LEARNING_RATE,
+        )
+        load_checkpoint(
+            CHECKPOINT_CRITIC, critic, opt_critic, LEARNING_RATE,
+        )
+        
+    gen.train()
+    critic.train()
+    step = int(log2(START_TRAIN_AT_IMG_SIZE / 4))
+    for num_epochs in PROGRESSIVE_EPOCHS[step:]:
+        alpha = 1e-5
+        loader, dataset = get_loader(4*2**step)
+        print(f"Image size: {4*2**step}")
+        
+        for epoch in range(num_epochs):
+            print(f"Epoch [{epoch+1}/{num_epochs}]")
+            alpha = train_fn(
+                critic,
+                gen,
+                loader,
+                dataset,
+                step,
+                alpha,
+                opt_critic,
+                opt_gen,
+                scalar_gen,
+                scalar_critic
+            )
+            
+            if SAVE_MODEL:
+                save_checkpoint(gen, opt_gen, filename=CHECKPOINT_GEN)
+                save_checkpoint(critic, opt_critic, filename=CHECKPOINT_CRITIC)
+                
+        step += 1 # progress to the next img size
+
+# The training begins!
+wandb.init(project='ImAProPainter')
+run_id = wandb.run.id
+torch.backends.cudnn.benchmarks = True # performance benefits!
+main()
